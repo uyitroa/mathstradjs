b675a6e7eacbf93d089d9d3ba61472ba908dce7b
shintaroDesu <snkraishin87@gmail.com>
2022-08-20 17:28:55 +0200

    add build

---
+++ build/precache-manifest.67859d285d07cb3e68abb1be18bbf9e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ef8d08506522abc31db",
+    "url": "/static/css/main.86ddddca.chunk.css"
+  },
+  {
+    "revision": "7ef8d08506522abc31db",
+    "url": "/static/js/main.7ef8d085.chunk.js"
+  },
+  {
+    "revision": "c6b5a3e0f92ee25b3986",
+    "url": "/static/js/1.c6b5a3e0.chunk.js"
+  },
+  {
+    "revision": "1588f8af7d3e22f20715",
+    "url": "/static/css/2.4e05c6cd.chunk.css"
+  },
+  {
+    "revision": "1588f8af7d3e22f20715",
+    "url": "/static/js/2.1588f8af.chunk.js"
+  },
+  {
+    "revision": "37673b42f4e1b2750cf5",
+    "url": "/static/js/runtime~main.37673b42.js"
+  },
+  {
+    "revision": "6a129670d6b184d5ded0d02e1873304c",
+    "url": "/index.html"
+  }
+];
