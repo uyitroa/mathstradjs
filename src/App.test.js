98a7b862b786cdbc2240cd0dbf2c8bc145a36100
shintaroDesu <snkraishin87@gmail.com>
2022-08-20 17:33:00 +0200

    add build

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {cleanParenthesis} from "./utils/translateUtils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// import App from './App';
+//
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("test describe", () => {
+  test("ok", () => {
+    expect(cleanParenthesis("Field (mathematics)")).toBe("Field");
+    expect(cleanParenthesis("dãy (toán)")).toBe("dãy");
+  })
+})
